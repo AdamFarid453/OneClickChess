343ff2607a9b24b09e0925d9a0f5463c0f7d8147
Adam Farid <afarid@stevens.edu>
2021-12-28 23:57:25 -0500

    added svg icons with hover ability

--- oneclickchess/src/components/LandingPage.js
+++ oneclickchess/src/components/LandingPage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import logo from "../images/logo.jpg";
 import play from "../images/play.jpg";
-import github from "../images/github.png";
-import medium from "../images/medium.png";
 const LandingPage = () => {
   return (
     <div>
@@ -12,14 +10,26 @@ const LandingPage = () => {
       <div className="play">
         <img src={play} alt="play" className="play" />
       </div>
-
-      <div className="socialmedias">
+      <div className="logos">
         <a href="https://www.github.com/adamfarid453">
-          <img src={github} alt="github" className="github" />
+          <svg
+            className="github"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z" />
+          </svg>
         </a>
-        <a href="https://medium.com/@adamfarid453">
-          <img src={medium} alt="medium" className="medium" />
-        </a>
+        <a href="https://medium.com/@adamfarid453"></a>
+        <svg
+          className="medium"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        >
+          <path d="M12 0c-6.626 0-12 5.372-12 12 0 6.627 5.374 12 12 12 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12zm6.974 17.557v-.235l-1.092-1.072c-.096-.073-.144-.194-.124-.313v-7.874c-.02-.119.028-.24.124-.313l1.118-1.072v-.235h-3.869l-2.758 6.88-3.138-6.88h-4.059v.235l1.308 1.575c.128.115.194.285.176.457v6.188c.038.223-.032.451-.189.614l-1.471 1.784v.235h4.17v-.235l-1.471-1.784c-.158-.163-.233-.389-.202-.614v-5.352l3.66 7.985h.425l3.143-7.985v6.365c0 .17 0 .202-.111.313l-1.13 1.098v.235h5.49z" />
+        </svg>
       </div>
     </div>
   );
